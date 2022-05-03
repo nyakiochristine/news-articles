3ba31ccdd378b9304a2ccac0ff5ebee43159ad35
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-03 20:08:36 +0300

    ft:added a function for getting news sources

--- app/models.py
+++ app/models.py
@@ -4,6 +4,15 @@ class Sources:
     defines source objects
     '''
     
+def _init_(self,id,name,description,url,category,country,language,):
+    self.id = id
+    self.name = name
+    self.description = description
+    self.url = url
+    self.category = category
+    self.country = country
+    self.language = language
+    
     
     
     
@@ -17,3 +26,4 @@ class Articles:
     '''
     defines articles objects
     '''  
+    
